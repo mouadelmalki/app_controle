e17a95d196b855bb42beaeb61bf914170ab8b168
unknown <mouadelmalki00@gmail.com>
2022-12-08 23:52:01 +0100

    init

---
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,29 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+
+    }
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(new Customer(null, "mouad","mouad@gmail.com"));
+            customerRepository.save(new Customer(null, "zakaria","zak@gmail.com"));
+            customerRepository.save(new Customer(null, "med","med@gmail.com"));
+            customerRepository.findAll().forEach(c ->{
+                System.out.println(c.toString());
+            } );
+        };
+    }
+
+}
